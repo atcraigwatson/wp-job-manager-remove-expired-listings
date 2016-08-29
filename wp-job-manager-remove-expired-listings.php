c9c3c0415c58ae62325b61a8cbf90d62a9dd73c9
Craig Waton <craig@rokkitpress.com>
2016-08-29 18:07:11 +0100

    First commit

---
+++ wp-job-manager-remove-expired-listings.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Plugin Name: WP Job Manager - Auto Delete Expired Listings
+ * Plugin URI: https://github.com/rokkitpress/
+ * Description: Lets you set a specific number of days to automatically delete listings from the database.
+ * Version: 1.0
+ * Author: Craig Watson
+ * Author URI: https://github.com/rokkitpress
+ * Requires at least: 4.1
+ * Tested up to: 4.6
+ * Text Domain: auto-delete-experied-listings
+ * Domain Path: /languages/
+ * License: GPL2+
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+//* Create admin submenu page under joblistings.
+add_action('admin_menu', 'wpjm_adel_menu');
+function wpjm_adel_menu() {
+	add_submenu_page(
+	  'edit.php?post_type=job_listing',
+		'Auto Delete Expired Job Listings',
+	  'Auto Delete Expired Job Listings',
+		'administrator',
+		'wpjm-adel-settings',
+		'wpjm_adel_settings_page'
+	);
+}
+
+//* Callback function to build settings page content.
+function wpjm_adel_settings_page() {
+	?>
+
+		<div class="wrap">
+		<h1>Auto Delete Job Listings</h1>
+		<p>By default WP Job Manager does not delete expired jobs from the server.</p>
+		<p>By setting a specificnumber of days bellow, the job listings will run for the set listings duration and then contiue to be stored in the users account for the set number of days. Once that is up the listings will be permenantly deleted from the database.</p>
+
+		<form method="post" action="options.php">
+			<?php settings_fields( 'wpjm-adel-settings-group' ); ?>
+			<?php do_settings_sections( 'wpjm-adel-settings-group' ); ?>
+			<table class="form-table">
+					<tr valign="top">
+					<th scope="row">Delete after this many days.</th>
+					<td><input type="number" name="wpjm_adel_num_days" value="<?php echo esc_attr( get_option('wpjm_adel_num_days') ); ?>" /></td>
+					</tr>
+			</table>
+
+			<?php submit_button(); ?>
+
+		</form>
+		</div>
+
+	<?php
+}
+
+//* Register the settings.
+add_action( 'admin_init', 'wpjm_adel_settings' );
+function wpjm_adel_settings() {
+	register_setting( 'wpjm-adel-settings-group', 'wpjm_adel_num_days' );
+}
+
+//* This will make sure expired jobs are deleted after XX days.
+add_filter( 'job_manager_delete_expired_jobs', '__return_true' );
+
+//* The default is 30 days, but you can change this with the following.
+add_filter( 'job_manager_delete_expired_jobs_days', 'change_job_manager_delete_expired_jobs_days' );
+function change_job_manager_delete_expired_jobs_days() {
+
+	$wpjm_adel_num_days = esc_attr( get_option( 'wpjm_adel_num_days' ) );
+
+  return $wpjm_adel_num_days;
+
+}
